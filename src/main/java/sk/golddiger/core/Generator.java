032d16293d1db9c6b3f5ae8903651a4d73a92f90
Martin Krajcovic <mar.krajc@gmail.com>
2022-06-14 23:05:14 +0200

    Generator quick test of generating types from descriptor

--- src/main/java/sk/golddiger/core/Generator.java
+++ src/main/java/sk/golddiger/core/Generator.java
@@ -1,11 +1,74 @@
 package sk.golddiger.core;
 
-public class Generator {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		
-		
-		
-	}
+public class Generator {
 	
+	public static void main(String[] args) {
+		/*
+	"ziadost_id": {
+        "type": [
+          "integer",
+          "null"
+        ]
+      },
+		 */
+		
+		// TODO: construct this better...line words one by one
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/sample_descriptors/person"))) {
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				// for now to test the types
+				if (line.contains("object") || line.contains("array")) {
+					continue;
+				}
+				String[] lineParts = line.split(" ");
+				String jsonPart = "";
+				switch(lineParts.length) {
+				case 2:
+					jsonPart = getJsonNamedTypeDesc(lineParts[0], lineParts[1], false, false, "");
+					break;
+				case 3:
+					jsonPart = getJsonNamedTypeDesc(lineParts[0], lineParts[1], false, false, lineParts[3]);
+					break;
+				case 4:
+					if (lineParts[2].equals("required")) {
+						jsonPart = getJsonNamedTypeDesc(lineParts[0], lineParts[1], true, false, lineParts[3]);
+					} else {
+						jsonPart = getJsonNamedTypeDesc(lineParts[0], lineParts[1], false, true, lineParts[3]);
+					}
+					break;
+				case 5:
+					jsonPart = getJsonNamedTypeDesc(lineParts[0], lineParts[1], true, true, lineParts[3]);
+				}
+				sb.append(jsonPart);
+			}		
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		System.out.println(sb);
+	}
+
+	// TODO: create jsonNamedArrayType
+	
+	// TODO: take level of tabs
+	private static String getJsonNamedTypeDesc(String type, String name, boolean required, boolean notNull, String lineEnding) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\"" + name + "\": {\n");
+		sb.append("\t" + "\"type\": [\n\t  \"" + type + "\"");
+		if (!notNull) {
+			sb.append(",\n\t  \"null\"\n\t]\n");
+		} else {
+			sb.append("\n\t]\n");
+		}
+		if (lineEnding.contains("\\")) {
+			sb.append("},\n");
+		} else {
+			sb.append("}\n");
+		}
+		return sb.toString();
+	}
 }
